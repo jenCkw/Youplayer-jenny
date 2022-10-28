19d0bc51cdd0ed17c81c8ac92cbc99c2c7e4701d
jenny <cikwaninejenny.com>
2022-10-28 12:56:43 +0100

    feat: Redesign authentication

--- src/App.js
+++ src/App.js
@@ -1,69 +1,96 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import NavBar from './components/NavBar';
-import Video from './components/Video';
-import { 
-  youtube_seach, 
-  clientId, 
-  API 
-} from './youtube'
-import { Routes, Route } from 'react-router-dom';
-import PlayVideo from './components/PlayVideo';
-import Auth from './components/Auth';
-import { gapi } from 'gapi-script';
+import { useEffect, useState } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Video from "./components/Video";
+import { youtube_seach, clientId, API } from "./youtube";
+import { Routes, Route } from "react-router-dom";
+import PlayVideo from "./components/PlayVideo";
+import Auth from "./components/Auth";
+import { gapi } from "gapi-script";
+import { useNavigate } from "react-router-dom";
 
-const token = localStorage.getItem('user');
+const token = localStorage.getItem("user");
 
 function App() {
-  const [videos, setVideos] = useState([])
+  const navigate = useNavigate();
+
+  const [videos, setVideos] = useState([]);
+
+  const [isConnected, setIsConnected] = useState(false);
+
   // onsearch function
   const search = async (keywork) => {
-    const response = await youtube_seach.get('/search',{
-      params:{
-        q: keywork
-      }
+    const response = await youtube_seach.get("/search", {
+      params: {
+        q: keywork,
+      },
     });
-    setVideos(response.data.items)
-  }
+    setVideos(response.data.items);
+  };
 
-  console.log(token)
+  console.log(token);
   // get subscription data
   const subscription = async () => {
-    fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&myRating=like&maxResults=50&key=${API}&access_token=${token}`)
-    .then(response=>response.json())
-    .then(data=>{setVideos(data.items)})
-  }
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&myRating=like&maxResults=50&key=${API}&access_token=${token}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setVideos(data.items);
+      });
+  };
 
-  const onSuccess = (res) =>{
-      console.log("LOGIN SUCCESS! Current user", res.profileObj);
-      localStorage.setItem("user",res.accessToken)
-  }
+  const onSuccess = (res) => {
+    console.log("LOGIN SUCCESS! Current user", res.profileObj);
+    localStorage.setItem("user", res.accessToken);
+    setIsConnected(true);
+    navigate("/");
+  };
 
-  const onFailure = (res) =>{
-      console.log("LOGIN SUCCESS! Current user", res.profileObj);
-  }
+  const onFailure = (res) => {
+    console.log("LOGIN SUCCESS! Current user", res.profileObj);
+  };
 
-  const start = () =>{
-    gapi.client.init({
-      apiKey: API,
-      clientId,
-      scope:'https://www.googleapis.com/auth/youtube.force-ssl',
-      discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
-    })
-  }
+  const start = () => {
+    gapi.client
+      .init({
+        apiKey: API,
+        clientId,
+        scope: "https://www.googleapis.com/auth/youtube",
+        discoveryDocs: [
+          "https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest",
+        ],
+      })
+      .then(() => {
+        var auth2 = gapi.auth2.getAuthInstance();
 
-  useEffect(()=>{
-    subscription()
-    gapi.load("client:auth2", start)
-  },[])
+        const user = auth2.currentUser.get();
+        setIsConnected(auth2.isSignedIn.get());
+      });
+  };
+
+  useEffect(() => {
+    gapi.load("client:auth2", start);
+  }, []);
+
+  useEffect(() => {
+    subscription();
+  }, [isConnected]);
 
   return (
     <div>
-      <NavBar search={search}/>
+      <NavBar
+        search={search}
+        isConnected={isConnected}
+        setIsConnected={setIsConnected}
+      />
       <Routes>
-        <Route path='/' element={<Video videos={videos}/>}/>
-        <Route path=':videoId' element={<PlayVideo videos={videos}/>}/>
-        <Route path='/login' element={<Auth onSuccess={onSuccess} onFailure={onFailure}/>}/>
+        <Route path="/" element={<Video videos={videos} />} />
+        <Route path=":videoId" element={<PlayVideo videos={videos} />} />
+        <Route
+          path="/login"
+          element={<Auth onSuccess={onSuccess} onFailure={onFailure} />}
+        />
       </Routes>
     </div>
   );
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,27 +1,50 @@
-import React from 'react'
-import './Navbar.css'
-import Search from '../Search'
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Navbar.css";
+import Search from "../Search";
+import { Link } from "react-router-dom";
+import { gapi } from "gapi-script";
+import { useNavigate } from "react-router-dom";
 
-function Nav({search}) {
+function Nav({ search, isConnected, setIsConnected }) {
   const [isactive, setIsactive] = React.useState(false);
-  
+  const navigate = useNavigate();
+
+  function logout() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      auth2.disconnect();
+      localStorage.clear();
+      setIsConnected(false);  
+      navigate("/login");
+    });
+  }
+
   return (
     <header>
-      <Link className='navLogo' to='/'>YouPlayer</Link>
-      <div className="menu" onClick={()=> setIsactive(!isactive)}>
+      <Link className="navLogo" to="/">
+        YouPlayer
+      </Link>
+      <div className="menu" onClick={() => setIsactive(!isactive)}>
         <div className="line"></div>
         <div className="line"></div>
         <div className="line"></div>
       </div>
-      <nav className={isactive ? `nav-active`: ``}>
-        <Search search={search} isactive={isactive}/>
-        <div>
-          <Link to="/login" className={isactive ? 'nav__login-active': 'nav__login'}>Login</Link>
-        </div>
+      <nav className={isactive ? `nav-active` : ``}>
+        <Search search={search} isactive={isactive} />
+        {isConnected && (
+          <div>
+            <Link
+              // to="/login"
+              onClick={logout}
+              className={isactive ? "nav__login-active" : "nav__login"}
+            >
+              Logout
+            </Link>
+          </div>
+        )}
       </nav>
     </header>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
