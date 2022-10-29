c7ecfcbbeaefb323fb3518568f0de322d1248539
jenny <cikwaninejenny.com>
2022-10-29 14:55:41 +0100

    fix: video card

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import Videocard from '../VideoCard'
-import './Video.css';
+import React from "react";
+import Videocard from "../VideoCard";
+import "./Video.css";
 // UC8butISFwT-Wl7EV0hUK0BQ
 function Video({ videos }) {
   return (
-    <div className='video'>
-      {
-        videos?.length === 0 ? (
-          <h1>Loading ....</h1>
-        ): videos?.map((video, i)=>(
-            <Videocard 
+    <div className="video">
+      {videos?.length === 0 ? (
+        <h1>Loading ....</h1>
+      ) : (
+        videos?.map((video, i) => (
+          <Videocard
             key={i}
-            videoId={video.id ? video.id : video.id.videoId }
+            videoId={typeof video.id === "string" ? video.id : video.id.videoId}
             thumnail={video.snippet.thumbnails.high.url}
             title={video.snippet.title}
             author={video.snippet.channelTitle}
-            />
-       ))
-      }
+          />
+        ))
+      )}
     </div>
-  )
+  );
 }
 
-export default Video
+export default Video;
